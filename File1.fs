af05cb479b6ad3c6564c1f4690804ecf7d8587f0
Amol Sahasrabudhe <amol.sahasrabudhe@gmail.com>
2014-01-14 23:57:06 +0530

    better findMaxDivisor

--- File1.fs
+++ File1.fs
@@ -3,32 +3,33 @@
 open System
 open System.Numerics
 open System.Collections.Generic
+open FsCheck
 type listMonadBuilder()=
         member x.Bind(comp,func)= comp|>List.map(fun x->func x)|> List.concat
         member x.Return(comp)=[comp]
-let mod2Test a b c= 0L=((1L+a+b+c) % 2L)
-let mod3Test a b c= (2L=b%3L) && (0L=(a+c)%3L)
+let mod2Test a b c= 0=((1+a+b+c) % 2)
+let mod3Test a b c= (2=b%3) && (0=(a+c)%3)
 let mod4Test a b c=
-    match a%4L with
-    |x when ((x=0L)||(x=2L))->( 0L=( (a+c)%4L) && (3L=b%4L) )   ||( (2L=(a+c)%4L)&&(1L=b%4L) )
+    match a%4 with
+    |x when ((x=0)||(x=2))->( 0=( (a+c)%4) && (3=b%4) )   ||( (2=(a+c)%4)&&(1=b%4) )
     |_->false 
 let mod8Test a b c=
-    match (a%8L,b%8L,c%8L) with
-    |x when x=(6L,3L,6L)->true
-    |x when x=(2L,7L,6L)->true
-    |x when x=(2L,3L,2L)->true
-    |x when x=(6L,7L,2L)->true 
+    match (a%8,b%8,c%8) with
+    |x when x=(6,3,6)->true
+    |x when x=(2,7,6)->true
+    |x when x=(2,3,2)->true
+    |x when x=(6,7,2)->true 
     |_->false
 let modnTest n a b c=
     match n with
-    |1L->true
-    |2L->mod2Test a b c
-    |3L->mod3Test a b c
-    |4L->mod4Test a b c
-    |6L->(mod2Test a b c) && (mod3Test a b c)
-    |8L->mod8Test a b c
-    |12L->(mod4Test a b c) && (mod3Test a b c)
-    |24L->(mod8Test a b c)&& (mod3Test a b  c)
+    |1->true
+    |2->mod2Test a b c
+    |3->mod3Test a b c
+    |4->mod4Test a b c
+    |6->(mod2Test a b c) && (mod3Test a b c)
+    |8->mod8Test a b c
+    |12->(mod4Test a b c) && (mod3Test a b c)
+    |24->(mod8Test a b c)&& (mod3Test a b  c)
     |_->false
 let poly x  a b c = (pown x 4) + (pown x 3)*a+(pown x 2)*b+x*c
 //let polyDivTest x a b c n= 0=(poly x a b c) % n
@@ -43,45 +44,59 @@ let inline boolToInt b=
     |true->int64 1
     |_->int64 0
 //let countIMod24TilM (m:int64)  (i:int64)=
-//    match m%(24L) with
-//    |x when x>=i->1L+m/24L- (boolToInt (i=0L))
-//    |_->m/24L-(boolToInt (i=0L))
+//    match m%(24) with
+//    |x when x>=i->1L+m/24- (boolToInt (i=0))
+//    |_->m/24-(boolToInt (i=0))
 
 
 
 let abcTriplets=
     listMonadBuilder(){
-        let!x=[0L..23L]
-        let!y=[0L..23L]
-        let!z=[0L..23L]
+        let!x=[0..23]
+        let!y=[0..23]
+        let!z=[0..23]
         return x,y,z}
-let findMaxDivisor  y=[|1L;2L;3L;4L;6L;8L;12L;24L|]|>Array.filter(fun x->y|||>modnTest x )|>(fun x->x.[x.Length-1])
+let findMaxDivisor  y=
+    let powersOf2 x=Seq.unfold(fun state->match (x|||>modnTest state ) with
+                                              |true->Some(state,2*state)
+                                              |false->None) 1
+                        |>Seq.last
+    let test3=match (y|||>modnTest 3)  with
+              |true->3
+              |false->1
+    test3*powersOf2 y
+//let a=findMaxDivisor (3,4,5L)
 let maxDivisorTriplet=abcTriplets|>List.map(fun x->findMaxDivisor x)
 let maxDivisorTripletMap=maxDivisorTriplet|>Seq.zip abcTriplets|>Seq.groupBy(fun (x,y)->y)|>Map.ofSeq|>Map.map(fun key value->Seq.map(fun x->fst x) value)
-let testMap n=maxDivisorTripletMap|>Map.map(fun key value->Seq.filter(fun (a,b,c)->(a<=n) && (b<=n) && (c<=n) && (a > 0L) && (b>0L) && (c>0L))  value)
+//let testMap n=maxDivisorTripletMap|>Map.map(fun key value->Seq.filter(fun (a,b,c)->(a<=n) && (b<=n) && (c<=n) && (a > 0L) && (b>0L) && (c>0L))  value)
                                   //|>Map.map(fun key value->Seq.length value)
                                   //|>Map.fold(fun acc key value->acc+key*value) 0
 let inline mod109 x= x%(pown 10L 9)
-let initState1=(0L,1L)
-let initState2=(0L,1L)
+let initState1=(0L,1)
+let initState2=(0L,1)
                
 let stateAdd (q1,r1) (q2,r2) = 
     let remTemp=(r1+r2)
     let rem=match remTemp with
-            |x when x>23L->((x-24L),true)
+            |x when x>23->((x-24),true)
             |_->(remTemp,false)
     let quotient = (q1+q2+(boolToInt (snd rem)))|>mod109
     (quotient,fst rem)
 //let dictIModN =
-let countNumTripletsPartial  state=
-    let numRemainderMap=[0L..23L]|>List.map(fun x->match x with 
-                                                   |y when y<=snd state->(y,1L-(boolToInt (y=0L) ) + fst state)
-                                                   |_->(x,fst state))|>Map.ofSeq
-    (fun (x,y,z)-> (numRemainderMap.[x]*numRemainderMap.[y]) |>mod109|>(fun x ->x*numRemainderMap.[z])|>mod109)
-let countNumTriplets (a,b,c) state= (countNumTripletsPartial state )(a,b,c)
+//let countNumTripletsPartial  state=
+//    let numRemainderArray=Array.init 24 (fun x->match x with 
+//                                                |y when y<=snd state->1L-(boolToInt (int64 y=0L) ) + fst state
+//                                                |_->fst state)
+//    (fun (x,y,z)-> (numRemainderArray.[x]*numRemainderArray.[ y]) |>mod109|>(fun x ->x*numRemainderArray.[z])|>mod109)
+let numRemainderFunc state x= 
+        match x with
+        |y when y=0->fst state
+        |y when y<=snd state->1L+fst state      
+        |_->fst state  
+//let countNumTriplets (a,b,c) state= (countNumTripletsPartial state )(a,b,c)
 let maxDivisorContribution state d  =
-    let numRemainderMap=countNumTripletsPartial state
-    maxDivisorTripletMap.[d]|>Seq.map(fun x->numRemainderMap x)
+    let numRemainderMap= numRemainderFunc state
+    maxDivisorTripletMap.[d]|>Seq.map(fun (a,b,c)->(numRemainderMap a)*numRemainderMap b|>mod109|>(fun x->x*numRemainderMap c)|>mod109)
     |>Seq.fold(fun acc x->mod109 (acc+x)) 0L 
-    |>(fun x-> x *d)|>mod109
+    |>(fun x-> x *int64 d)|>mod109
 
--- Library1.fs
+++ Library1.fs
@@ -2,7 +2,7 @@
 open System
 open System.Numerics
 module main=
-    let divisorList=[2L;3L;4L;6L;8L;12L;24L]
+    let divisorList=[1;2;3;4;6;8;12;24]
     //let test=divisorList|>List.map(fun x->(divTest.maxDivisorContribution (1L,0L) x)/x)//|>List.sum
     //printf "testAns=%A\n\r" test
     let findSum y=divisorList|>List.map(fun x->divTest.maxDivisorContribution y x)|>List.fold(fun acc x->divTest.mod109 (acc+x)) 0L
@@ -11,7 +11,7 @@ module main=
         if(n<123456L) then Some (prev ,(nextFib,prev,n+1L))
         else None) (divTest.initState1,divTest.initState2,1L)
     //Seq.iter(fun x->printf "%A\n\r" x)fibSeq
-    printf "testAns%A\n\r" (findSum (10000L/24L,10000L%24L))
+    printf "testAns%A\n\r" (findSum (10000L/24L,int (10000L%24L)))
     //let finalAns=Seq.map(fun x->findSum x) fibSeq
     let finalAns=Seq.fold(fun acc x->divTest.mod109 (acc+findSum x)) 0L fibSeq
     //Seq.iter(fun x->printf "%A\n\r" x ) finalAns
@@ -19,4 +19,4 @@ module main=
     //let testIntermediate=divTest.maxDivisorTripletMap.[4]
     //let dummy=Seq.iter(fun x->printf "%A\n\r"x ) testIntermediate
     //printf "test=%A" test10
-    System.Console.ReadLine()|>ignore
+    System.Console.ReadLine()|>ignore 
